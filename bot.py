31210bf441d7a871e5351707a2e4a46481b2f3aa
RaySCS <rayaan.siddiqi@gmail.com>
2020-07-09 21:11:35 -0500

    Starting covid bot

--- bot.py
+++ bot.py
@@ -21,15 +21,28 @@ async def on_message(message):
     if "wave to me" in message.content:
         await message.add_reaction('👋')
 
-    messageKey = "kakashi"
-    if messageKey in message.content:
+    if "how are you doing" in message.content:
+        await message.channel.send("Nothing much")
+
+    if "cat" in message.content:
         async with aiohttp.ClientSession() as session:
-            async with session.get('http://aws.random.cat/',messageKey) as r:
+            async with session.get('http://aws.random.cat/meow') as r:
                 if r.status == 200:
                     json = await r.json()
                     await message.channel.send(json['file'])
 
-token_file = open("token", "r")
+    #actual below
+    #covid: il
+    if "covid" in message.content:
+        print(message.content)
+        stateDescribed = message.content[7:len(message.content)]
+        await message.channel.send("you described " + stateDescribed)
+
+
+token_file = open("token", "r")#Load in the token file
 contents = token_file.read()
+
+#user types 'covid: state'
+#bot returns the number of cases
 
 client.run(contents)
