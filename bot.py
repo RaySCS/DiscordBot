d07953cd6e56e55913a4ba06e6f45b7c86ad2f9d
RaySCS <rayaan.siddiqi@gmail.com>
2020-07-11 16:09:03 -0500

    Syntax fixed

--- bot.py
+++ bot.py
@@ -46,7 +46,7 @@ async def on_message(message):
        await message.channel.send("Please Follow the Syntax: covid [state abbreviation or name]")
        await message.channel.send("Once you enter which state, you will be returned with information regarding the COVID-19 Situation in your state")
 
-   if "covid" in message.content or "Covid" in message.content or "COVID" in message.content or "hero" in message.content or "Hero" in message.content or "HERO" in message.content:
+   if "covid" in message.content or "Covid" in message.content or "COVID" in message.content:
        print(message.content)
        stateDescribed = message.content[7:len(message.content)]
        # await message.channel.send("you described " + stateDescribed)
@@ -89,7 +89,54 @@ async def on_message(message):
            await message.channel.send(str(stateDescribed) + " Citizens Recovered Today:  " + str(allStateInfo['recovered']))
        else:
            await message.channel.send("Please re-type your command.")
-           await message.channel.send("Plesae Follow the Syntax: Covid [state abbreviation or name] in order to get the information regarding the COVID-19 Situation in your state")
+
+   if "hero" in message.content or "Hero" in message.content or "HERO" in message.content:
+       print(message.content)
+       stateDescribed = message.content[6:len(message.content)]
+       # await message.channel.send("you described " + stateDescribed)
+
+       url = "https://covidtracking.com/api/states"
+
+       payload = {}
+       headers = {}
+
+       response = requests.request("GET", url, headers=headers, data=payload)
+
+       print(response.text.encode('utf8'))
+       foundState = False
+
+       to_python_JSONFILE = json.loads(response.text.encode('utf8'))
+       indexOfState = -1
+       for x in range(0, len(allUSStatesArray)):
+           if stateDescribed == allUSStatesArray[x]:
+               foundState = True
+               indexOfState = x
+               print("Index found: " + str(indexOfState))
+
+       if not foundState:
+           for x in range(0, len(allUSStatesSpelledOutArray)):
+               if stateDescribed == allUSStatesSpelledOutArray[x]:
+                   foundState = True
+                   indexOfState = x
+                   print("Index found: " + str(indexOfState))
+
+       if foundState:
+           print(to_python_JSONFILE[indexOfState])
+           allStateInfo = to_python_JSONFILE[indexOfState]
+           print(allStateInfo['positive'])
+           dateFormated = today.strftime("%b-%d-%Y")  # date format
+           await message.channel.send("`" + str(stateDescribed + " ALL INFORMATION AS OF " + dateFormated) + "`")
+           await message.channel.send("-----------------------------------")
+           await message.channel.send(str(stateDescribed) + " Total Positive Cases:  " + str(allStateInfo['positive']))
+           await message.channel.send(str(stateDescribed) + " Total Negative Cases:  " + str(allStateInfo['negative']))
+           await message.channel.send(
+               str(stateDescribed) + " Citizens Hospitalized Currently:  " + str(allStateInfo['hospitalizedCurrently']))
+           await message.channel.send(
+               str(stateDescribed) + " Citizens Recovered Today:  " + str(allStateInfo['recovered']))
+       else:
+           await message.channel.send("Please re-type your command.")
+           await message.channel.send(
+               "Plesae Follow the Syntax: Covid [state abbreviation or name] in order to get the information regarding the COVID-19 Situation in your state")
 token_file = open("token", "r")#Load in the token file
 contents = token_file.read()
 
